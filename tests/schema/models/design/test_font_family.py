92a206a49197050b296cec9d7b8a0b3da66c7055
Dhruv Jani <djani@Dhruvs-MacBook-Air.local>
2026-06-02 17:45:24 -0400

    initial changes

--- tests/schema/models/design/test_font_family.py
+++ tests/schema/models/design/test_font_family.py
@@ -1,4 +1,3 @@
-import pytest
 import rendercv_fonts
 
 from cvforge.schema.models.design.font_family import available_font_families
@@ -13,7 +12,7 @@ typst_built_in_font_families = {
 
 @pytest.mark.parametrize(
     "font_family",
-    [f for f in cvforge_fonts.available_font_families if f not in icon_font_families],
+    [f for f in rendercv_fonts.available_font_families if f not in icon_font_families],
 )
 def test_bundled_fonts_are_in_available_font_families(font_family):
     assert font_family in available_font_families
@@ -24,4 +23,4 @@ def test_bundled_fonts_are_in_available_font_families(font_family):
     [f for f in available_font_families if f not in typst_built_in_font_families],
 )
 def test_no_extra_fonts_in_available_font_families(font_family):
-    assert font_family in cvforge_fonts.available_font_families
+    assert font_family in rendercv_fonts.available_font_families
